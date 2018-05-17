4cc76a93b9426fe5250a3b0c6e7796e43e70ec27
rbuisson <romain@mekomsolutions.com>
2018-05-17 15:08:48 +0200

    TLC-100: Configure Reporting Age Range via SQL script
    	modified:   configuration/scripts/post_run/configureAgeRanges.sql

--- configuration/scripts/post_run/configureAgeRanges.sql
+++ configuration/scripts/post_run/configureAgeRanges.sql
@@ -1,2 +1,6 @@
-insert into reporting_age_group values (2, "<14", "Adult Child", 0,0,14,-1,1);
-insert into reporting_age_group values (3, "14+", "Adult Child", 14,0,200,0,2);
+delete from reporting_age_group;
+insert into reporting_age_group ( id, name, report_group_name, min_years, min_days, max_years, max_days, sort_order ) values (1, "All", "All Ages", 0,0,200,-0,0);
+insert into reporting_age_group ( id, name, report_group_name, min_years, min_days, max_years, max_days, sort_order ) values (2, "Ch <14", "All Ages", 0,0,14,-1,1);
+insert into reporting_age_group ( id, name, report_group_name, min_years, min_days, max_years, max_days, sort_order ) values (3, "Ad 14+", "All Ages", 14,0,200,0,2);
+insert into reporting_age_group ( id, name, report_group_name, min_years, min_days, max_years, max_days, sort_order ) values (4, "Ch <14", "Adult Child", 0,0,14,-1,1);
+insert into reporting_age_group ( id, name, report_group_name, min_years, min_days, max_years, max_days, sort_order ) values (5, "Ad 14+", "Adult Child", 14,0,200,0,2);
